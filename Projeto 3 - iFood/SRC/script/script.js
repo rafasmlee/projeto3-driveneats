64be5c7765bdf91fcff3334f982e73803e0547bc
rafasmlee <114461097+rafasmlee@users.noreply.github.com>
2023-04-03 17:10:42 -0300

    Add files via upload

--- Projeto 3 - iFood/SRC/script/script.js
+++ Projeto 3 - iFood/SRC/script/script.js
@@ -1,4 +1,23 @@
+let pedidoEscolhido = '';
+let pedidoEscolhido2 = '';
+let pedidoEscolhido3 = '';
+
+function verificaSelecao(){
+    if (pedidoEscolhido !== '') {
+        if (pedidoEscolhido2 !==''){
+            if(pedidoEscolhido3 !==''){
+                const botaoFinalizar = document.querySelector('.oculto')
+                fim.classList.classList.toggle('oculto')
+            }
+        }
+        
+    }
+    
+}
+
 function selecionarComida(seletor){
+
+     pedidoEscolhido = seletor.innerHTML;
 
     const botaoSelecionadoAnteriormente = document.querySelector('.selecionado');
     if(botaoSelecionadoAnteriormente != null){
@@ -7,29 +26,40 @@ function selecionarComida(seletor){
 
     const botao = document.querySelector(seletor);
     botao.classList.add('selecionado');
+
+    verificaSelecao();
 
 }
 
 function selecionarBebida(seletor){
 
-    const botaoSelecionadoAnteriormente = document.querySelector('.selecionado');
+    pedidoEscolhido2 = seletor.innerHTML;
+
+
+    const botaoSelecionadoAnteriormente = document.querySelector('.selecionado2');
     if(botaoSelecionadoAnteriormente != null){
-        botaoSelecionadoAnteriormente.classList.remove('selecionado')
+        botaoSelecionadoAnteriormente.classList.remove('selecionado2')
     }
 
     const botao = document.querySelector(seletor);
-    botao.classList.add('selecionado');
+    botao.classList.add('selecionado2');
+
+    verificaSelecao();
 
 }
 
 function selecionarSobremesa(seletor){
 
-    const botaoSelecionadoAnteriormente = document.querySelector('.selecionado');
+    pedidoEscolhido3 = seletor.innerHTML;
+
+
+    const botaoSelecionadoAnteriormente = document.querySelector('.selecionado3');
     if(botaoSelecionadoAnteriormente != null){
-        botaoSelecionadoAnteriormente.classList.remove('selecionado')
+        botaoSelecionadoAnteriormente.classList.remove('selecionado3')
     }
 
     const botao = document.querySelector(seletor);
-    botao.classList.add('selecionado');
+    botao.classList.add('selecionado3');
 
+    verificaSelecao();
 }
